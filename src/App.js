4a8808196ccb1b24d4477aef5729971f7e2b41c9
Talon Revis <113617774+beaconweb3@users.noreply.github.com>
2023-05-08 15:30:21 -0500

    created fetchCharacter function in App and basic layout for Character.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// import ReactDOM, { createRoot } from 'react-dom'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +9,28 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+const [charData, setCharData] = useState({char: 'empty', birth_year: 'empty', gender: 'empty', height: 'empty'});
+
+
+  const fetchChar = (id) =>
+  fetch(`https://swapi.dev/api/people/`)
+    .then( receivedData => receivedData.json())
+    .then( charData => {
+      charData.map( obj => {
+        console.log(obj);
+      })
+      
+    })
+
+
+
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <button onClick={ (e) => fetchChar(1)}>Get Luke Info</button>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
-// Write your Character component here
+import React, { useState } from "react";
+import App from "App"
+
+
+
+
+export function Characters(props) {
+    const {chosenOne} = props;
+
+
+    return 
+    <div>
+        <h2>{chosenOne.name}</h2>
+        <p>Gender: {chosenOne.gender}</p>
+        <p>Birth Year: {chosenOne.birth_year}</p>
+        <p>Height: {chosenOne.height}</p>
+        <p>Weight: {chosenOne.mass}</p>
+        <p>Homeworld: {chosenOne[homeworld].name}</p>
+    </div>
+}
